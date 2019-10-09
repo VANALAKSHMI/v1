9d7d125faa29376acc17ed174faba38be8a95901
Vanalakshmi P <41038161+VANALAKSHMI@users.noreply.github.com>
2019-10-09 15:41:06 +0530

    Create v516.py

---
+++ v516.py
@@ -0,0 +1,20 @@
+#co-prime
+n=int(input()
+      )
+m=int(input()
+      )
+c=0
+d=0
+for x in range(1,n+1):
+    s=n%x
+    if(s==0):
+        c=c+1
+for x in range(1,m+1):
+    s=m%x
+    if(s==0):
+        d=d+1
+if(c==2)&(d==2):
+    print("1")
+else:
+    print("0")
+ 
